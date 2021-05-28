e2d525a22323883e43cb0a8e5ab5e442d375a66e
yongchangdong <yongchang.dong@gmail.com>
2021-05-28 16:23:31 -0700

    tweets model

--- twitter/settings.py
+++ twitter/settings.py
@@ -38,8 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'rest_framework',
+    'debug_toolbar',
 
+    'rest_framework',
+    'tweets',
     'accounts',
 ]
 
@@ -49,6 +51,7 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -131,3 +134,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+INTERNAL_IPS = ['10.0.2.2']

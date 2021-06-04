058dbaa3beac5d2d502e276d255cee3a060d359f
yongchangdong <yongchang.dong@gmail.com>
2021-06-03 17:32:15 -0700

    friendships

--- twitter/urls.py
+++ twitter/urls.py
@@ -16,10 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
-from django.conf import settings
+
 from accounts.api.views import UserViewSet, AccountViewSet
 from tweets.api.views import TweetViewSet
-import debug_toolbar
 from friendships.api.views import FriendshipViewSet
 
 router = routers.DefaultRouter()
@@ -32,11 +31,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('__debug__/', include(debug_toolbar.urls)),
 ]
-
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns.append(
-        path('__debug__', include(debug_toolbar.urls))
-    )
